6a4787afb0045fb213ae7eb5b010593eaf182573
aghadiayeamayanvboernest <161714003+aghadiayeamayanvboernest@users.noreply.github.com>
2024-06-12 18:55:03 +0100

    adding C# prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,63 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            Console.WriteLine("Guess the magic number! It is between 1 and 204.");
+            Console.WriteLine();
+            
+            //Console.Write("Enter the magic number: ");
+            //string magicNumberInput = Console.ReadLine();
+            //int magicNumber = int.Parse(magicNumberInput);
+
+            // Randomly generate the magic number betwee 1 and 204
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 205);
+
+            //Initialize guessMagicNumber to a value that is not equa to the magic number
+            int guessMagicNumber = -1;
+
+            int numberOfGuesses = 0;
+
+            //This line of code keep asking for guesses untill the user guesses the magic number 
+            while(guessMagicNumber != magicNumber)
+            {
+                // This line f Code Increment the number of guesses
+                numberOfGuesses++;
+
+                //This set of code prompt the user to guess the magic number
+                Console.Write("Guess the magic number: ");
+                string guessMagicnumberInput = Console.ReadLine();
+                guessMagicNumber = int.Parse(guessMagicnumberInput);
+
+                //This set of ode determine if the user guess is too high, too low, or correct
+                if (guessMagicNumber > magicNumber)
+                {
+                    Console.WriteLine("You need to Aim Lower");
+                }
+                else if (guessMagicNumber < magicNumber)
+                {
+                    Console.WriteLine("You need to Aim Higher");
+                }
+                else 
+                {
+                    Console.WriteLine("That's it! You guessed the magic number!");
+                }
+            } 
+
+            Console.WriteLine();
+            // This line of Code Inform the User about the total Number of guesses they made 
+            Console.WriteLine($"You made a total of {numberOfGuesses} guesses.");
+
+            Console.WriteLine();
+            // This line of code prompt the user if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine(); 
+        }
+
     }
-}
+}    
