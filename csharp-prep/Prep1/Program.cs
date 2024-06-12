5fdaa872f0afb2d914c7986e326d6ee34d80b66a
aghadiayeamayanvboernest <ama24004@byui.edu>
2024-06-12 05:07:11 +0100

    csharp prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask the user for their name. 
+        Console.WriteLine();
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        // this line of code capitalize the first letter of the user input
+        string capitalizedFirstName = firstname.Substring(0, 1).ToUpper() + firstname.Substring(1).ToLower();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+        // this line of code capitalize the first letter of the user input
+        string capitalizedlastname = lastname.Substring(0,1).ToUpper() + lastname.Substring(1).ToLower();
+        
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {capitalizedlastname}, {capitalizedFirstName} {capitalizedlastname}");
+        Console.WriteLine();
+    
     }
-}
+}    
