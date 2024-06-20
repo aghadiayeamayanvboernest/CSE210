3e14a6d01f819356a98992922edb6fc5d688eaae
aghadiayeamayanvboernest <161714003+aghadiayeamayanvboernest@users.noreply.github.com>
2024-06-20 04:05:56 +0100

    learning 02 update

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -8,18 +8,21 @@ class Program
     {   
         //Create the first job instance
         Job job1 = new Job();
+        {
         job1._jobTitle ="User experience (UX) designer";
         job1._companyName = "Apple";
         job1._startYear = 2010;
         job1._endYear = 2017;
+        }
 
         // Create the second job instance 
         Job job2 = new Job();
-        job2. _jobTitle = "Web Developer";
-        job2._companyName = "Amazon";
-        job2._startYear = 2017;
-        job2._endYear = 2023;
-
+        {
+            job2. _jobTitle = "Web Developer";
+            job2._companyName = "Amazon";
+            job2._startYear = 2017;
+            job2._endYear = 2023;
+        }
         // create a new resume instance 
         Resume userResume = new Resume();
         userResume._userName ="Aghadiaye Amayanvbo Ernest";
