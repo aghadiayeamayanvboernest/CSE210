8cc626a514becf839fa62a6308ee7c2d99dd5cc0
aghadiayeamayanvboernest <161714003+aghadiayeamayanvboernest@users.noreply.github.com>
2024-07-10 12:47:35 +0100

    learning up date

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Test Assignment class
+        Assignment assignment = new Assignment("Aghadiaye Ernest", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        Console.WriteLine();
+        // Test MathAssignment class
+        MathAssignment mathAssignment = new MathAssignment("Ernest Jacob", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine();
+        // Test WritingAssignment class
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
+        Console.WriteLine();
     }
 }
